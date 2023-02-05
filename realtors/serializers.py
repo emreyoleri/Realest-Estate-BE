3202cf63353cf3f979d154c9d117c671aa907a02
Emre Yoleri <emreyoleri01@hotmail.com>
2023-02-05 18:31:03 +0300

    auto complete error 😓

--- realtors/serializers.py
+++ realtors/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Realtor
 
-class RealtorSerializer(serializers.ModelField):
+class RealtorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Realtor
         fields = "__all__"
