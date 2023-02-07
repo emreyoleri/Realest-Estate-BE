1a63843f1b94ddffcfff9f3d5c67575c149f8f2e
Emre Yoleri <emreyoleri01@hotmail.com>
2023-02-08 01:55:24 +0300

    update set_house

--- scripts/fake_data.py
+++ scripts/fake_data.py
@@ -1,7 +1,5 @@
 import base64
 from django.contrib.auth.models import User
-from listings.serializers import ListingSerializer
-from accounts.models import UserAccount
 from faker import Faker
 
 import django
@@ -9,8 +7,13 @@ import os
 import random
 import requests
 
+import json
+
 from django.contrib.auth import get_user_model
 from slugify import slugify
+
+import base64
+
 
 User = get_user_model()
 
@@ -19,32 +22,22 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realest_estate.settings')
 
 django.setup()
 
-# HOME_IMAGES = [
-#     'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-#     'https://www.bhg.com/thmb/0Fg0imFSA6HVZMS2DFWPvjbYDoQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg',
-#     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_DL2uFBGanoPOo9VFQdRUfmb8N-mHxf268eqUmhkXtM9_r0qBGaYBsZt4pGCtiwd4Og&usqp=CAU'
-#     'https://t3.ftcdn.net/jpg/05/62/86/46/360_F_562864607_3Q3F4auiwahzzZPBJSpEGJ1xjV00Ii3w.jpg'
-#     'https://res.cloudinary.com/brickandbatten/image/upload/c_scale,w_464,h_324,dpr_2/f_auto,q_auto/v1641000863/wordpress_assets/22826-ModContemporary-Accents_w-GauntletGray-a-ok.jpg?_i=AA'
-# ]
 
 
 def set_house():
     fake = Faker(['en_US'])
 
-    for i in range(0, 5):
+    for i in range(0, 50):
 
         response = requests.get(
-            "https://pixabay.com/api/?key=22161424-08421d893c81fc01d6d419611&q=house+architecture&image_type=photo")
+            "https://pixabay.com/api/?key=22161424-08421d893c81fc01d6d419611&q=house+architecture&image_type=photo&per_page=100")
 
         jsn = response.json()
 
         url = jsn["hits"][i]["webformatURL"]
 
-        # def get_as_base64():
-
-        #     return base64.b64encode(requests.get(url).content)
-
         address = fake.street_address()
+        
         data = dict(
             realtor=random.randint(1, User.objects.count()),
             slug=slugify(address),
@@ -53,54 +46,22 @@ def set_house():
             city=fake.city(),
             state=fake.country(),
             zipcode=fake.postcode(),
-            description=fake.paragraph(nb_sentences=5),
-            sale_type="For Sale",
-            price=random.randint(3, 12) * 10 ** 4,
-            bedrooms=random.randint(1, 5),
-            bathrooms=random.randint(1, 5),
-            sqft=random.randint(1, 20) * 10**2,
+            description=fake.paragraph(nb_sentences=7),
+            sale_type="For Sale" if fake.pybool() else "For Rent",
+            price=random.randint(10, 50) * 10 ** 4,
+            bedrooms=random.randint(2, 7),
+            bathrooms=random.randint(2, 7),
+            sqft=random.randint(10, 20) * 10**2,
             open_house=fake.pybool(),
-            photo_main=base64.b64encode(requests.get(url).content)
+            photo_main=str(base64.b64encode(
+                requests.get(url).content).decode('ASCII'))
         )
+        json_object = json.dumps(data, indent=4)
 
-        serializer = ListingSerializer(data=data)
-        if i == 0:
-            print(data)
+        res = requests.post("http://127.0.0.1:8000/api/listings/", headers={
+                            'Content-Type': 'application/json', 'charset': 'utf-8'}, data=json_object)
+        data = res.json()
 
-        print("****************************")
-        if serializer.is_valid():
-            serializer.save()
-            print('home saved')
-        else:
-            continue
+        print("data", data)
 
-
-# def get_books(key_word):
-#     fake = Faker(['en_US'])
-#     url = 'http://openlibrary.org/search.json'
-#     payload = {'q': key_word}
-#     response = requests.get(url, params=payload)
-
-#     if response.status_code != 200:
-#         print('Bad request made', response.status_code)
-#         return
-
-#     jsn = response.json()
-#     books = jsn.get('docs')
-
-#     for book in books:
-#         book_name = book.get('title')
-#         data = dict(
-#             name=book_name,
-#             author=book.get('author_name')[0],
-#             description='-'.join(book.get('author_facet')),
-#             date_of_relase=fake.date_time_between(
-#                 start_date='-10y', end_date='now', tzinfo=None),
-#         )
-
-#         serializer = BookSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             print('book saved: ', book_name)
-#         else:
-#             continue
+        
