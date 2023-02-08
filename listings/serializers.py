91aeb7c121750855f0a6c615eef28cd4f5793b67
Emre Yoleri <emreyoleri01@hotmail.com>
2023-02-08 03:18:29 +0300

    restore codes v2

--- listings/serializers.py
+++ listings/serializers.py
@@ -4,7 +4,7 @@ from .models import Listing
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = "__all__"
+        fields = ('title', 'address', 'city', 'state', 'price', 'sale_type', 'home_type', 'bedrooms', 'bathrooms', 'sqft', 'photo_main', 'slug')
 
 class ListingDetailSerializer(serializers.ModelSerializer):
     class Meta:
--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView , CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import permissions
 from .models import Listing
 from .serializers import ListingSerializer, ListingDetailSerializer
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timezone
 
-class ListingsView(ListAPIView , CreateAPIView):
+class ListingsView(ListAPIView):
     queryset = Listing.objects.order_by('-list_date').filter(is_published=True)
     permission_classes = (permissions.AllowAny, )
     serializer_class = ListingSerializer
